2acbd271b8f6153eb61c046aa61a1cab5901de04
Md Yasin Hossain Akash <mdyhakash@gmail.com>
2026-07-11 14:19:33 +0600

    fix: category admin only

--- src/modules/category/category.route.ts
+++ src/modules/category/category.route.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { categoryController } from "./category.controller";
+import { auth } from "../../middleware/auth";
+import { Role } from "../../../generated/prisma/enums";
 
 const router = Router();
 
-router.post("/", categoryController.createCategory);
+router.post("/", auth(Role.ADMIN), categoryController.createCategory);
 router.get("/", categoryController.getAllCategory);
 router.get("/:id", categoryController.getCategoryById);
-router.patch("/:id", categoryController.updateCategory);
-router.delete("/:id", categoryController.deleteCategory);
+router.patch("/:id", auth(Role.ADMIN), categoryController.updateCategory);
+router.delete("/:id", auth(Role.ADMIN), categoryController.deleteCategory);
 
 export const categoryRoutes = router;
